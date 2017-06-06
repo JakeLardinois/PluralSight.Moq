961a38b12287bf2b50a08d3eaec584552f0c3aed
JakeLardinois <jake.lardinois@gmail.com>
2017-06-06 07:35:15 -0500

    Added Demo 3 Notes

--- Tests/Demo03/CustomerServiceTests.cs
+++ Tests/Demo03/CustomerServiceTests.cs
@@ -11,27 +11,29 @@ namespace PluralSight.Moq.Tests.Demo03
         {
             //this shows how setting the return value will change the execution flow
             [Test]
-            //[ExpectedException(typeof(InvalidCustomerMailingAddressException))]
             public void an_exception_should_be_thrown_if_the_address_is_not_created()
             {
-                //Arrange
+                /**Arrange**/
+
                 var customerToCreateDto = new CustomerToCreateDto 
                             {FirstName = "Bob", LastName = "Builder"};
                 var mockAddressBuilder = new Mock<ICustomerAddressBuilder>();
                 var mockCustomerRepository = new Mock<ICustomerRepository>();
 
+                //Set up the addressbuilder so that when you call its .From method and pass it any customerDto, that it will
+                //return a null (which in this case will be a null Address)
                 mockAddressBuilder
                     .Setup(x => x.From(It.IsAny<CustomerToCreateDto>()))
                     .Returns(() => null);
+                    //.Returns(() => new Address()); //Would make the test fail...
 
                 var customerService = new CustomerService(
                     mockAddressBuilder.Object, 
                     mockCustomerRepository.Object);
-                
-                //Act
-                //customerService.Create(customerToCreateDto);
 
-                //Assert
+                /**Act**/
+                /**Assert**/
+
                 Assert.That(() => customerService.Create(customerToCreateDto),
                 Throws.TypeOf<InvalidCustomerMailingAddressException>());
             }
@@ -39,21 +41,26 @@ namespace PluralSight.Moq.Tests.Demo03
             [Test]
             public void the_customer_should_be_saved_if_the_address_was_created()
             {
-                //Arrange
+                /**Arrange**/
+
                 var customerToCreateDto = new CustomerToCreateDto { FirstName = "Bob", LastName = "Builder" };
                 var mockAddressBuilder = new Mock<ICustomerAddressBuilder>();
                 var mockCustomerRepository = new Mock<ICustomerRepository>();
 
+                //Set up the address builder to return a new Address object when you call its .From method and pass it any customerDto
                 mockAddressBuilder
                     .Setup(x => x.From(It.IsAny<CustomerToCreateDto>()))
                     .Returns(() => new Address());
+                    //.Returns(() => null); //Would make the test fail...
 
                 var customerService = new CustomerService(mockAddressBuilder.Object, mockCustomerRepository.Object);
 
-                //Act
+                /**Act**/
+
                 customerService.Create(customerToCreateDto);
 
-                //Assert
+                /**Assert**/
+
                 mockCustomerRepository.Verify(y=>y.Save(It.IsAny<Customer>()));
             }
         }
