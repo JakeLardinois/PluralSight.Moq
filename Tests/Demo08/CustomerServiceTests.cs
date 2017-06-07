a86043573110c309f4bc7537db9dacdbfed40e2c
JakeLardinois <jake.lardinois@gmail.com>
2017-06-07 11:46:34 -0500

    Demo 8 Notes

--- Tests/Demo08/CustomerServiceTests.cs
+++ Tests/Demo08/CustomerServiceTests.cs
@@ -10,27 +10,28 @@ namespace PluralSight.Moq.Tests.Demo08
         public class When_creating_a_customer_which_has_an_invalid_address
         {
             [Test]
-            //[ExpectedException(typeof(CustomerCreationException))]
             public void an_exception_should_be_raised()
             {
-                //Arrange
+                /**Arrange**/
+
                 var mockCustomerRepository = new Mock<ICustomerRepository>();
                 var mockCustomerAddressFactory = new Mock<ICustomerAddressFactory>();
 
                 mockCustomerAddressFactory
-                    .Setup(x => x.From(It.IsAny<CustomerToCreateDto>()))
-                    .Throws<InvalidCustomerAddressException>();
+                    .Setup(x => x.From(It.IsAny<CustomerToCreateDto>())) //When any CustomerToCreateDto is passed into the From Method of the CustomerAddressFactory Class
+                    .Throws<InvalidCustomerAddressException>(); //an InvalidCustomerAddressException is thrown...
 
                 var customerService = new CustomerService(
                     mockCustomerRepository.Object, 
                     mockCustomerAddressFactory.Object);
 
-                //Act
-                //customerService.Create(new CustomerToCreateDto());
+                /**Act**/
+                /**Assert**/
 
-                //Assert
+                //In the CustomerService Class there is a Try Catch that catches the above InvalidCustomerAddressException and then throws a CustomerCreationException
+                //That we are testing for here...
                 Assert.That(() => customerService.Create(new CustomerToCreateDto()),
-                Throws.TypeOf<CustomerCreationException>());
+                    Throws.TypeOf<CustomerCreationException>());
             }
         }
     }
