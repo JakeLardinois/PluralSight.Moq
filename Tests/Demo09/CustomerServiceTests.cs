1e3a364ec4ccad57faafda5db822dfd0b58df549
JakeLardinois <jake.lardinois@gmail.com>
2017-06-07 11:51:23 -0500

    Demo 9 Notes

--- Tests/Demo09/CustomerServiceTests.cs
+++ Tests/Demo09/CustomerServiceTests.cs
@@ -12,18 +12,23 @@ namespace PluralSight.Moq.Tests.Demo09
             [Test]
             public void the_local_timezone_should_be_set()
             {
-                //Arrange
+                /**Arrange**/
+
+                //We want to test that the LocalTimeZone property has been set on the CustomerRepository Class in the CustomerService Class.
+
                 var mockCustomerRepository = new Mock<ICustomerRepository>();
 
                 var customerService = new CustomerService(
                     mockCustomerRepository.Object);
 
-                //Act
+                /**Act**/
+
                 customerService.Create(new CustomerToCreateDto());
 
-                //Assert
-                mockCustomerRepository.VerifySet(
-                    x=>x.LocalTimeZone = It.IsAny<string>());
+                /**Assert**/
+
+                mockCustomerRepository.VerifySet( //this method is used to verify that LocalTimeZone on the CustomerRepository object is set.
+                    x =>x.LocalTimeZone = It.IsAny<string>());
             }
         }
     }
