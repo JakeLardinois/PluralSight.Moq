56fad78647c4515c1d6d96c85aa509d20b6397a6
JakeLardinois <jake.lardinois@gmail.com>
2017-06-08 07:16:40 -0500

    Demo 13 Notes

--- Code/Demo13/CustomerService.cs
+++ Code/Demo13/CustomerService.cs
@@ -12,12 +12,13 @@
             _customerRepository = customerRepository;
             _mailingRepository = mailingRepository;
 
+            //Wire up the event so that then the NotifySalesTeam event fires, the NotifySalesTeam method is fired.
             _customerRepository.NotifySalesTeam += NotifySalesTeam;
         }
 
         private void NotifySalesTeam(object sender, NotifySalesTeamEventArgs e)
         {
-            _mailingRepository.NewCustomerMessage(e.Name);
+            _mailingRepository.NewCustomerMessage(e.Name);  //We are testing if this method gets executed.
         }
 
         public void Create(CustomerToCreateDto customerToCreate)
--- Tests/Demo13/CustomerServiceTests.cs
+++ Tests/Demo13/CustomerServiceTests.cs
@@ -12,19 +12,27 @@ namespace PluralSight.Moq.Tests.Demo13
             [Test]
             public void an_email_should_be_sent_to_the_sales_team()
             {
-                //Arrange
+                /**Arrange**/
+
+                //Testing an Event.
+
                 var mockCustomerRepository = new Mock<ICustomerRepository>();
                 var mockMailingRepository = new Mock<IMailingRepository>();
 
                 var customerService = new CustomerService(
                     mockCustomerRepository.Object, mockMailingRepository.Object);
 
-                //Act
-                mockCustomerRepository.Raise(
-                    x=>x.NotifySalesTeam += null,
-                    new NotifySalesTeamEventArgs("jim"));
+                /**Act**/
 
-                //Assert
+                mockCustomerRepository.Raise( //raises an event
+                    x=>x.NotifySalesTeam += null,  //we want to raise the NotifySalesTeam event (null could be some type of handler; object property?)
+                    new NotifySalesTeamEventArgs("jim")); //pass in the event arguments. Note the custom implementation of EventArgs, so that the constructor takes in a parameter
+                                                          //which sets a Name property on NotifySalesTeamEventArgs...
+
+                /**Assert**/
+
+                //We want to verify that the NewCustomerMessage method of the MailingRepository object is called and we don't care what value of 
+                //parameter (string) being passed in is.
                 mockMailingRepository.Verify(
                     x => x.NewCustomerMessage(It.IsAny<string>()));
             }
