9ab513ead57dce1e041b948162ec8f57b49a2649
JakeLardinois <jake.lardinois@gmail.com>
2017-06-08 06:58:14 -0500

    Demo 12 Notes

--- Tests/Demo12/CustomerServiceTests.cs
+++ Tests/Demo12/CustomerServiceTests.cs
@@ -12,10 +12,23 @@ namespace PluralSight.Moq.Tests.Demo12
             [Test]
             public void the_workstation_id_should_be_retrieved()
             {
-                //Arrange
+                /**Arrange**/
+
+                //Take a look at stubbing out properties on Mock objects and setting initial values for those properties.
+
+
                 var mockCustomerRepository = new Mock<ICustomerRepository>();
                 var mockApplicationSettings = new Mock<IApplicationSettings>();
 
+                //Sets the WorkstationId as a stub property.
+                //mockApplicationSettings.SetupProperty(x => x.WorkstationId);
+                //Sets the return value.
+                //mockApplicationSettings.Object.WorkstationId = 2345;
+
+                //Sets the WorkstationId as a stub property AND sets the initial return value.
+                //mockApplicationSettings.SetupProperty(x => x.WorkstationId, 1234);
+
+                //This says that every property on that Mockable object is stubbable and can be set
                 mockApplicationSettings.SetupAllProperties();
                 mockApplicationSettings.Object.WorkstationId = 2345;
 
@@ -24,10 +37,12 @@ namespace PluralSight.Moq.Tests.Demo12
                     mockCustomerRepository.Object, 
                     mockApplicationSettings.Object);
 
-                //Act
+                /**Act**/
+
                 customerService.Create(new CustomerToCreateDto());
 
-                //Assert
+                /**Assert**/
+
                 mockApplicationSettings.VerifyGet(x=>x.WorkstationId);
             }
         }
