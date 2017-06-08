482c657deca8fe7c29964c820234c8fb5a1c5ad6
JakeLardinois <jake.lardinois@gmail.com>
2017-06-08 06:32:18 -0500

    Demo 11 Notes

--- Tests/Demo11/CustomerServiceTests.cs
+++ Tests/Demo11/CustomerServiceTests.cs
@@ -12,20 +12,31 @@ namespace PluralSight.Moq.Tests.Demo11
             [Test]
             public void the_workstationid_should_be_retrieved()
             {
-                //Arrange
+                /**Arrange**/
+
+                //Testing the scenario where there is a set of deep nested objects that needs to be tested.
+                //Mockable objects can be automatically created for Interfaces and Classes with Virtual Methods & Properties on them
+
+
                 var mockCustomerRepository = new Mock<ICustomerRepository>();
                 var mockApplicationSettings = new Mock<IApplicationSettings>();
 
+                /*In other frameworks, what you would need to do is Mock out the ApplicationSettings and have it return a mock object when the SystemConfiguration is called
+                 * and have that return a mock object when its AuditingInformation property is called and have that AuditingInformation mock object return the WorkstationId...
+                 * But in the Mock framework, its MUCH easier... The mock framework automatically creates the mock objects for the below; it realizes the below objects are mockable
+                 * because in this case they are interfaces*/
                 mockApplicationSettings.Setup(
                     x => x.SystemConfiguration.AuditingInformation.WorkstationId)
                     .Returns(123);
                 
                 var customerService = new CustomerService(mockCustomerRepository.Object, mockApplicationSettings.Object);
 
-                //Act
+                /**Act**/
+
                 customerService.Create(new CustomerToCreateDto());
 
-                //Assert
+                /**Assert**/
+
                 mockApplicationSettings.VerifyGet(
                     x => x.SystemConfiguration.AuditingInformation.WorkstationId);
             }
