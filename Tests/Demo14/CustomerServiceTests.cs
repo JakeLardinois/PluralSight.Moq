04601058de722261581e8e08d4d4d8b51c0c829f
JakeLardinois <jake.lardinois@gmail.com>
2017-06-08 08:04:43 -0500

    Demo 14 Notes

--- Tests/Demo14/CustomerServiceTests.cs
+++ Tests/Demo14/CustomerServiceTests.cs
@@ -12,19 +12,31 @@ namespace PluralSight.Moq.Tests.Demo14
             [Test]
             public void the_customer_should_be_saved()
             {
-                //Arrange
-                var mockCustomerRepository = 
+                /**Arrange**/
+
+                //Differences between Strict and Loose behaviour in the Moq Framework.
+
+                //This will cause the test to fail because inside of CustomerService the FetchAll method is called against the CustomerRepository and there
+                //isn't an explicit setup called for the FetchAll method...
+                var mockCustomerRepository =
                     new Mock<ICustomerRepository>(MockBehavior.Strict);
 
+                //Here the test will pass because the default behaviour of a Moq object is Loose
+                //var mockCustomerRepository =
+                //    new Mock<ICustomerRepository>();
+
+                //Explicit setup for the Save method of the CustomerRepository
                 mockCustomerRepository.Setup(x => x.Save(It.IsAny<Customer>()));
 
                 var customerService = new CustomerService(
                     mockCustomerRepository.Object);
 
-                //Act
+                /**Act**/
+
                 customerService.Create(new CustomerToCreateDto());
 
-                //Assert
+                /**Assert**/
+
                 mockCustomerRepository.Verify();
             }
         }
